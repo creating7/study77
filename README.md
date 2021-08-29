d7ebc16c09bf1372cb401192486fc074a21973af
creating7 <89498460+creating7@users.noreply.github.com>
2021-08-29 02:14:22 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+![ci](https://github.com/bndw/wifi-card/workflows/ci/badge.svg)
+
+# 📶 WiFi Card
+
+https://wificard.io
+
+Print a simple card with your WiFi login details. Tape it to the fridge, keep it in your wallet, etc.
+
+![wificard](https://user-images.githubusercontent.com/4248167/125180713-2f5d3d00-e1b2-11eb-88f8-be5b4db4e0b1.gif)
+
+## Running locally
+
+Run the official Docker image on http://localhost:8080
+
+```
+make run
+```
+
+Run the live-reload server on http://localhost:3000
+
+```
+make dev
+```
---
+++ src/components/Card.js
@@ -0,0 +1,121 @@
+import QRCode from 'qrcode.react';
+import { useEffect, useRef, useState } from 'react';
+import './style.css';
+
+export const Card = () => {
+  const firstLoad = useRef(true);
+  const [qrvalue, setQrvalue] = useState('');
+  const [network, setNetwork] = useState({
+    ssid: '',
+    password: '',
+  });
+  const [portrait, setPortrait] = useState(false);
+
+  const escape = (v) => {
+    const needsEscape = ['"', ';', ',', ':', '\\'];
+
+    let escaped = '';
+    for (let i = 0; i < v.length; i++) {
+      let c = v[i];
+      if (needsEscape.includes(c)) {
+        c = '\\' + c;
+      }
+      escaped += c;
+    }
+
+    return escaped;
+  };
+
+  const onPrint = () => {
+    if (network.password.length < 8) {
+      alert('Password must be at least 8 characters');
+    } else {
+      window.print();
+    }
+  };
+
+  useEffect(() => {
+    if (firstLoad.current && window.innerWidth < 500) {
+      firstLoad.current = false;
+      setPortrait(true);
+    }
+
+    const ssid = escape(network.ssid);
+    const password = escape(network.password);
+    setQrvalue(`WIFI:T:WPA;S:${ssid};P:${password};;`);
+  }, [network]);
+
+  return (
+    <div>
+      <fieldset
+        id="print-area"
+        style={{ maxWidth: portrait ? '350px' : '100%' }}
+      >
+        <h1 style={{ textAlign: portrait ? 'center' : 'left' }}>WiFi Login</h1>
+
+        <div
+          className="details"
+          style={{ flexDirection: portrait ? 'column' : 'row' }}
+        >
+          <QRCode
+            className="qrcode"
+            style={{ paddingRight: portrait ? '' : '1em' }}
+            value={qrvalue}
+            size={175}
+          />
+
+          <div className="inputs">
+            <label>Network name</label>
+            <textarea
+              id="ssid"
+              type="text"
+              maxLength="32"
+              placeholder="WiFi Network name"
+              autoComplete="off"
+              autoCorrect="off"
+              autoCapitalize="none"
+              spellCheck="false"
+              value={network.ssid}
+              onChange={(e) => setNetwork({ ...network, ssid: e.target.value })}
+            />
+            <label>Password</label>
+            <textarea
+              id="password"
+              type="text"
+              style={{
+                height:
+                  portrait && network.password.length > 40 ? '5em' : 'auto',
+              }}
+              maxLength="63"
+              placeholder="Password"
+              autoComplete="off"
+              autoCorrect="off"
+              autoCapitalize="none"
+              spellCheck="false"
+              value={network.password}
+              onChange={(e) =>
+                setNetwork({ ...network, password: e.target.value })
+              }
+            />
+          </div>
+        </div>
+
+        <p>
+          <span role="img" aria-label="mobile-phone">
+            📸📱
+          </span>
+          Point your phone's camera at the QR Code to connect automatically
+        </p>
+      </fieldset>
+
+      <div className="buttons">
+        <button id="rotate" onClick={() => setPortrait(!portrait)}>
+          Rotate
+        </button>
+        <button id="print" onClick={onPrint}>
+          Print
+        </button>
+      </div>
+    </div>
+  );
+};
